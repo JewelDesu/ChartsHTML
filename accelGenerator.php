dbd0e2c0ff4f94a76bdf1e3c5005091150dd2971
Crystic <vilius817@gmail.com>
2025-08-18 13:46:18 +0300

    modified:   accelGenerator.php

--- accelGenerator.php
+++ accelGenerator.php
@@ -6,7 +6,7 @@ $array = array();
 $filepath = "/sys/class/thermal/thermal_zone0/temp";
 $filedata = file_get_contents($filepath);
 
-$array['xA'] = rand(100);
+$array['xA'] = rand(100, 1);
 header('Content-type: application/json');
 echo json_encode($array);
 
