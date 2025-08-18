e14f87f6464b323b257c7495f1583bc1ce7baec1
Crystic <vilius817@gmail.com>
2025-08-18 13:13:53 +0300

    first commit

---
+++ accelGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+  
+$array = array();
+
+$filepath = "/sys/class/thermal/thermal_zone0/temp";
+$filedata = file_get_contents($filepath);
+
+$array['xA'] = rand(100);
+header('Content-type: application/json');
+echo json_encode($array);
+
+?>
