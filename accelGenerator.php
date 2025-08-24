b36cca2344ee69068b9f09b0e9829159a6619c56
Crystic <vilius817@gmail.com>
2025-08-24 13:10:44 +0300

    Oled Test

--- accelGenerator.php
+++ accelGenerator.php
@@ -5,8 +5,16 @@ $array = array();
 
 $filepath = "/sys/class/thermal/thermal_zone0/temp";
 $filedata = file_get_contents($filepath);
+$writefile = fopen("/dev/ttyRPMSG0","w");
 
-$array['xA'] = $filedata / 1000;
+$val = $filedata / 1000;
+
+fwrite($writefile, $val);
+fclose($writefile);
+
+$array['xA'] = $val;
+
+
 header('Content-type: application/json');
 echo json_encode($array);
 
